ae05b7baa73036609db13f93fda840a3b0e8ab06
ASUKA-LANGLEY-SOHRYU <mishaprosvirninmail@yandex.ru>
2024-04-23 20:02:00 +0500

    Has been added page hello.html + MainController has been changed.

--- src/main/java/com/example/afisha/controllers/MainController.java
+++ src/main/java/com/example/afisha/controllers/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
     public String getMainPage(){
         return "index";
     }
+
+    @GetMapping("/hello")
+    public String getHelloPage(){
+        return "hello";
+    }
 }
