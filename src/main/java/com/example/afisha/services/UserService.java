f1bcc192cb7002c38b5af407b87cd761ecb9c54c
ASUKA-LANGLEY-SOHRYU <mishaprosvirninmail@yandex.ru>
2024-04-24 20:42:51 +0500

    UserService updated.

--- src/main/java/com/example/afisha/services/UserService.java
+++ src/main/java/com/example/afisha/services/UserService.java
@@ -1,5 +1,28 @@
 package com.example.afisha.services;
 
+import com.example.afisha.models.User;
+import com.example.afisha.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByFirstName(String firstname) {
+        var user = userRepository.findByFirstName(firstname);
+        if (user != null)
+            System.out.println(user);
+        return user;
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
