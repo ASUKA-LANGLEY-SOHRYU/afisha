b57855518eed6b11c427de18f157d70dd8749fda
ASUKA-LANGLEY-SOHRYU <mishaprosvirninmail@yandex.ru>
2024-04-24 20:42:15 +0500

    repositories added.

---
+++ src/main/java/com/example/afisha/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.example.afisha.repository;
+
+import com.example.afisha.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
