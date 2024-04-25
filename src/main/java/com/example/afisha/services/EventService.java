5d6b615dc7eeb3ae1646d8490f6a8ccd5f5b745e
Lena <pjanvalena333@gmail.com>
2024-04-25 23:34:49 +0500

    updated links to css

--- src/main/java/com/example/afisha/services/EventService.java
+++ src/main/java/com/example/afisha/services/EventService.java
@@ -21,5 +21,5 @@ public class EventService {
         return eventRepository.findAll();
     }
 
-    public List<Event> getEventsSortedByDate() { return eventRepository.findAll(Sort.by("dateTime").descending()); }
+    public List<Event> getEventsSortedByDate() { return eventRepository.findAll(Sort.by("dateTime").ascending()); }
 }
