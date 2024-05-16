2012a8ff694737da246721926bc316ec93976db2
Lena <pjanvalena333@gmail.com>
2024-05-16 18:20:33 +0500

    added styles for login and registration, added requests

--- src/main/java/com/example/afisha/controllers/CartController.java
+++ src/main/java/com/example/afisha/controllers/CartController.java
@@ -2,10 +2,12 @@ package com.example.afisha.controllers;
 
 import com.example.afisha.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class CartController {
@@ -21,9 +23,10 @@ public class CartController {
         return "../frontend/cart";
     }
 
-    @PostMapping("/cart/add")
-    public String addEventToCart(){
-
-        return null;
+    // убрать
+    @PostMapping("/addToCart")
+    public ResponseEntity<String> addEventToCart(@RequestBody String eventData){
+        System.out.println("Event data received: " + eventData);
+        return ResponseEntity.ok("Event added to cart successfully");
     }
 }
