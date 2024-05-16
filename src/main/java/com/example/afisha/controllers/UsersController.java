91825ca57f678b28b84686f10fc12e65b669b1aa
ASUKA-LANGLEY-SOHRYU <mishaprosvirninmail@yandex.ru>
2024-05-16 16:47:10 +0500

    Added profile endpoint

--- src/main/java/com/example/afisha/controllers/UsersController.java
+++ src/main/java/com/example/afisha/controllers/UsersController.java
@@ -1,10 +1,13 @@
 package com.example.afisha.controllers;
 
+import com.example.afisha.models.User;
 import com.example.afisha.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UsersController {
@@ -22,8 +25,16 @@ public class UsersController {
         return "../frontend/users";
     }
 
-    @GetMapping("/profile")
-    public String getProfilePage(Model model){
+    @GetMapping("/users/me")
+    public String getMyProfilePage(Model model, Authentication authentication){
+        model.addAttribute("user", (User) authentication.getPrincipal());
+        model.addAttribute("example2", "test2");
+        model.addAttribute("userEvents", userService.getUserOrders());
+        return "../frontend/profile";
+    }
+
+    @GetMapping("/users/{id}}")
+    public String getProfilePage(Model model, @PathVariable("id") Long id){
         model.addAttribute("example2", "test2");
         model.addAttribute("userEvents", userService.getUserOrders());
         return "../frontend/profile";
