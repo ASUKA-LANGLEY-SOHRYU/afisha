fa5a825078a0ac406fe87cb0c609681bce2ef518
ASUKA-LANGLEY-SOHRYU <mishaprosvirninmail@yandex.ru>
2024-06-06 08:13:43 +0500

    orders fix

--- src/main/java/com/example/afisha/models/User.java
+++ src/main/java/com/example/afisha/models/User.java
@@ -89,5 +89,9 @@ public class User implements UserDetails {
         return true;
     }
 
+    @Transient
+    public String getFullName(){
+        return lastName + " " + firstName + " " + patronymic;
+    }
 
 }
