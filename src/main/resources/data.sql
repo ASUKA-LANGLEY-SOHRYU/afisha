e93fdbbbd6f30f6cd2322f3ddaf2ab0375e54ddc
Lena <pjanvalena333@gmail.com>
2024-06-06 17:52:19 +0500

    fixed sql and some design

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,12 +3,17 @@ INSERT INTO event (price, name, date_time, organization_id) VALUES (200, 'Test E
 INSERT INTO event (price, name, date_time, organization_id) VALUES (200, 'Test Event 3', '2024-01-02 12:00:00', 1);
 
 
-INSERT INTO usr (last_name, first_name, patronymic, phone_number, email, birth_date, password) VALUES ('Иванов', 'Иван', 'Иванович', '1234567890', 'test0@example.com', '2000-01-01', '123');
-INSERT INTO user_role (user_id, role) VALUES ((SELECT id FROM usr WHERE email = 'test0@example.com'), 'ROLE_ORGANIZER');
-INSERT INTO usr (last_name, first_name, patronymic, phone_number, email, birth_date, password) VALUES ('Петров', 'Петр', 'Петрович', '1234567890', 'test2@example.com', '2001-01-01', '123');
-INSERT INTO user_role (user_id, role) VALUES ((SELECT id FROM usr WHERE email = 'test2@example.com'), 'ROLE_USER');
-INSERT INTO usr (last_name, first_name, patronymic, phone_number, email, birth_date, password) VALUES ('Михайлов', 'Михаил', 'Михайлович', '1234567890', 'test3@example.com', '2002-02-02', '123');
-INSERT INTO user_role (user_id, role) VALUES ((SELECT id FROM usr WHERE email = 'test3@example.com'), 'ROLE_ADMIN');
+INSERT INTO usr (last_name, first_name, patronymic, phone_number, email, birth_date, password)
+VALUES
+    ('Иванов', 'Иван', 'Иванович', '1234567890', 'test0@example.com', '2000-01-01', '123'),
+    ('Петров', 'Петр', 'Петрович', '1234567890', 'test2@example.com', '2001-01-01', '123'),
+    ('Михайлов', 'Михаил', 'Михайлович', '1234567890', 'test3@example.com', '2002-02-02', '123');
+
+INSERT INTO user_role (user_id, roles)
+VALUES
+    ((SELECT id FROM usr WHERE email = 'test0@example.com'), 'ORGANIZER'),
+    ((SELECT id FROM usr WHERE email = 'test2@example.com'), 'USER'),
+    ((SELECT id FROM usr WHERE email = 'test3@example.com'), 'ADMIN');
 
 
 INSERT INTO ordr (user_id, event_id) VALUES (1, 1);
